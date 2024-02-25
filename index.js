988cdc309a22eccc3037d78d408b06c741643c1b
Peter Kwok <peter.kwok94@gmail.com>
2024-02-25 18:01:24 -0500

    feat: shorturl route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParse = require('body-parser')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +11,27 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+// my code
+app.use('/api', (req, res, next) => {
+  next()
+})
+
+app.post('/api/shorturl', (req, res) => {
+  res.json({
+    original_url: null,
+    short_url: null
+  })
+})
